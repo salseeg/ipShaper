699ab741806b3e68aa9fcbf6ff922fc03b255f9e
Pavel Goncharov <pavel@altairnet.org>
2012-08-13 11:11:34 +0300

    ?????????? ????????? ????? ??

--- _core/network.php
+++ _core/network.php
@@ -63,7 +63,7 @@ Network::$ranges['89.185.16.0/21'] = new ipv4RangeCalc('89.185.16.0',21);
 Network::$ranges['93.185.222.0/23'] = new ipv4RangeCalc('93.185.222.0',23);
 Network::$ranges['93.185.216.0/23'] = new ipv4RangeCalc('93.185.216.0',23);
 Network::$ranges['93.185.218.0/23'] = new ipv4RangeCalc('93.185.218.0',23);
-Network::$ranges['93.185.220.0/23'] = new ipv4RangeCalc('93.185.218.0',23);
+Network::$ranges['93.185.220.0/23'] = new ipv4RangeCalc('93.185.220.0',23);
 
 
 ?>
