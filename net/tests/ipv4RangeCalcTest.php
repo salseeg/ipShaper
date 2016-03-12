de7a608dc9be85b677305fdcf77ae4fcd40c1f94
Sergey Lukianov <salseeg@gmail.com>
2016-03-12 21:57:42 +0200

    clearing code

--- net/tests/ipv4RangeCalcTest.php
+++ net/tests/ipv4RangeCalcTest.php
@@ -16,15 +16,15 @@ class ipv4RangeCalcTest extends \PHPUnit_Framework_TestCase
 {
 
     function testBasic(){
-        $calc = new ipv4RangeCalc('89.185.10.200', 25);
+        $calc = new ipv4RangeCalc('192.168.0.200', 25);
 
         $netIp = $calc->get_net_ip();
-        $this->assertEquals("89.185.10.128", $netIp);
-        $this->assertEquals("89.185.10.129", $calc->get_gate_ip());
+        $this->assertEquals("192.168.0.128", $netIp);
+        $this->assertEquals("192.168.0.129", $calc->get_gate_ip());
 
         $this->assertEquals("255.255.255.128", $calc->get_mask());
-        $this->assertEquals("89.185.10.255", $calc->get_broadcast_ip());
-        $this->assertEquals("89.185.10.130 - 89.185.10.254", $calc->get_abons_ips_as_range());
+        $this->assertEquals("192.168.0.255", $calc->get_broadcast_ip());
+        $this->assertEquals("192.168.0.130 - 192.168.0.254", $calc->get_abons_ips_as_range());
 
     }
 
